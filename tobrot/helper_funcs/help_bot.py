d143d43bc322e219b0f93d1690523d0f578e5e62
ACHIYA96 <39017907+ACHIYA96@users.noreply.github.com>
2020-07-24 15:19:49 +0530

    Add files via upload

---
+++ tobrot/helper_funcs/help_bot.py
@@ -0,0 +1,53 @@
+@@ -0,0 +1,52 @@
+................Available Commands....................
+
+/ytdl@achi: This command should be used as reply to a supported link
+
+/ytdl gdrive: This will download and upload to your cloud.
+
+/leech: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [this command will SPAM the chat and send the downloads a seperate files, if there is more than one file, in the specified torrent]
+
+/leech archive: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [This command will create a .tar.gz file of the output directory, and send the files in the chat, splited into PARTS of 1024MiB each, due to Telegram limitations]
+
+/gleech@achi: This command should be used as reply to a magnetic link, a torrent link, or a direct link. And this will download the files from the given link or torrent and will upload to the cloud using rclone.
+
+/gleech archive This command will compress the folder/file and will upload to your cloud.
+
+/leech unzip: This will unzip the .zip file and dupload to telegram.
+
+/gleech unzip: This will unzip the .zip file and upload to cloud.
+
+/leech unrar: This will unrar the .rar file and dupload to telegram.
+
+/gleech unrar: This will unrar the .rar file and upload to cloud.
+
+/leech untar: This will untar the .tar file and upload to telegram.
+
+/gleech untar: This will untar the .tar file and upload to cloud..
+
+/tleech: This will mirror the telegram files to ur respective cloud cloud.
+
+/tleech unzip: This will unzip the .zip telegram file and upload to cloud.
+
+/tleech unrar: This will unrar the .rar telegram file and upload to cloud.
+
+/tleech untar: This will untar the .tar telegram file and upload to cloud.
+
+
+---------------------NEW ADD ............................
+
+🔥/savethumbnail
+
+🔥/status
+
+🔥/clearthumbnail
+
+🔥/status: Shows a status of all the downloads
+
+🔥/stats: Show Stats of the machine the bot is hosted on
+
+🔥/ping: Test Ping 
+
+🔥/getsize: to check the size of this bot destination folder on gdrive
+
+🔥/index: to get the index link where all files are stored
